e57e8db33d83f46f0c82fc9fde524869eede4927
VANDINHHOAI <vanhhh332@gmail.com>
2024-01-22 15:58:03 +0700

    first commit

---
+++ app/src/main/java/com/example/tablayouttutorial/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.tablayouttutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.widget.TableLayout;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout mTabLayout;
+    private ViewPager mviewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTabLayout = findViewById(R.id.tab_layout);
+        mviewPager = findViewById(R.id.view_pager);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        mviewPager.setAdapter(viewPagerAdapter);
+
+        mTabLayout.setupWithViewPager(mviewPager);
+    }
+}
